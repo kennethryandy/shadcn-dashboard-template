5ed160ee2dc7fce370738f214c2aff74816fb607
kennethryandy <dykennethryan@gmail.com>
2024-03-26 12:34:31 +0800

    😀added icon

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,35 +1,23 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import LogoSvg from "./logo-svg";
 
 // ----------------------------------------------------------------------
 
 interface ILogoProps {
-  className?: string;
-  height?: number;
-  width?: number;
+	className?: string;
 }
 
-export default function Logo({
-  className,
-  height = 40,
-  width = 40,
-}: ILogoProps) {
-  const logo = (
-    <div
-      className={cn(
-        "inline-flex h-10 w-10",
-        className,
-        height && `h-[${height}px]`,
-        width && `w-[${width}px]`
-      )}
-    >
-      <Image src="/vercel.svg" alt="Logo" height={height} width={width} />
-    </div>
-  );
-  return (
-    <Link href="/" className="contents">
-      {logo}
-    </Link>
-  );
+export default function Logo({ className }: ILogoProps) {
+	const logo = (
+		<div className={cn("inline-flex h-14 w-14", className)}>
+			<LogoSvg />
+		</div>
+	);
+	return (
+		<Link href="/" className="contents m-0">
+			{logo}
+		</Link>
+	);
 }
